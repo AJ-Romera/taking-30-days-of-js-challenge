59661f74723a8a5fbaf5a23baed3d46491ec8129
AJ-Romera <ajromeraalfonso@gmail.com>
2020-10-16 19:52:34 +0200

    Day 3 exercises

---
+++ day-3-exercises/exercises1.js
@@ -0,0 +1,41 @@
+// 1. Declare firstName, lastName, country, city, age, isMarried, year variable and assign value to it and use the typeof operator to check different data types.
+let firstName = 'Antonio José';
+typeof...
+// 2. Check if type of '10' is equal to 10
+// 3. Check if parseInt('9.8') is equal to 10
+// 4. Boolean value is either true or false.
+    /* i. Write three JavaScript statement which provide truthy value.
+    ii. Write three JavaScript statement which provide falsy value. */
+// 5. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+    /* i. 4 > 3
+    ii. 4 >= 3
+    iii. 4 < 3
+    iv. 4 <= 3
+    v. 4 == 4
+    vi. 4 === 4
+    vii. 4 != 4
+    viii. 4 !== 4
+    ix. 4 != '4'
+    x. 4 == '4'
+    xi. 4 === '4'
+    xii. Find the length of python and jargon and make a falsy comparison statement. */
+// 6. Figure out the result of the following expressions first without using console.log(). After you decide the result confirm it by using console.log()
+    /* i. 4 > 3 && 10 < 12
+    ii. 4 > 3 && 10 > 12
+    iii. 4 > 3 || 10 < 12
+    iv. 4 > 3 || 10 > 12
+    v. !(4 > 3)
+    vi. !(4 < 3)
+    vii. !(false)
+    viii. !(4 > 3 && 10 < 12)
+    ix. !(4 > 3 && 10 > 12)
+    x. !(4 === '4')
+    xi. There is no 'on' in both dragon and python */
+// 7. Use the Date object to do the following activities
+    /* i. What is the year today?
+    ii. What is the month today as a number?
+    iii. What is the date today?
+    iv. What is the day today as a number?
+    v. What is the hours now?
+    vi. What is the minutes now?
+    vii. Find out the numbers of seconds elapsed from January 1, 1970 to now. */
