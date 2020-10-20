387f77d9f154b4596c750a32a167d23c62f9e47e
AJ-Romera <ajromeraalfonso@gmail.com>
2020-10-20 20:06:50 +0200

    Finish day 5 level 2 exercises

--- day-5-exercises/main.js
+++ day-5-exercises/main.js
@@ -2,9 +2,9 @@
 
 // 1. Declare an empty array;
 
-const emptyArray = Array();
-// or... let emptyArray = new Array();
-// but better use... const emptyArray = []
+/* const emptyArray = Array();
+or... let emptyArray = new Array();
+but better use... const emptyArray = [] /*
 
 // 2. Declare an array with more than 5 number of elements
 
@@ -163,44 +163,155 @@ console.log(itCompanies[6].includes("oo")); // False
 
 // 15. Sort the array using sort() method
 
-console.log(itCompanies.sort());
+// console.log(itCompanies.sort());
 
 // 16. Reverse the array using reverse() method
 
-console.log(itCompanies.reverse());
+// console.log(itCompanies.reverse());
 
 // 17. Slice out the first 3 companies from the array
 
-console.log(itCompanies.slice(0, 3));
+// console.log(itCompanies.slice(0, 3));
 
 // 18. Slice out the last 3 companies from the array
 
-console.log(itCompanies.slice(itCompanies.length - 3, itCompanies.length));
+// console.log(itCompanies.slice(itCompanies.length - 3, itCompanies.length));
 
 // 19. Slice out the middle IT company or companies from the array
 
-middleIndex // middleCompany from the exercise 9 contains the middle company index
-console.log(itCompanies.slice(middleIndex, middleIndex+1));
+/* middleIndex // middleCompany from the exercise 9 contains the middle company index
+console.log(itCompanies.slice(middleIndex, middleIndex+1)); */
 
 // 20. Remove the first IT company from the array
 
-itCompanies.shift();
-console.log(itCompanies);
+/* itCompanies.shift();
+console.log(itCompanies); */
 
 // 21. Remove the middle IT company or companies from the array
     /* We have:
     ["Microsoft", "IBM", "Google", "Facebook", "Apple", "Amazon"]
     at the moment, so let´s remove "Google" and "Facebook". */
 
-itCompanies.splice(2, 2); // From index 2 removes 2 elements including the element in index 2
-console.log(itCompanies);
+/* itCompanies.splice(2, 2); // From index 2 removes 2 elements including the element in index 2
+console.log(itCompanies); */
 
 // 22. Remove the last IT company from the array
 
-itCompanies.pop()
-console.log(itCompanies);
+/* itCompanies.pop()
+console.log(itCompanies); */
 
 // 23. Remove all IT companies
 
-itCompanies.splice(0)
-console.log(itCompanies);
+/* itCompanies.splice(0)
+console.log(itCompanies); */
+
+
+// Exercises: Level 2
+
+// 1. Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+    // Done
+
+// 2. First remove all the punctuations and change the string to array and count the number of words in the array
+
+/* let text =
+'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+console.log(words)
+console.log(words.length)
+["I", "love", "teaching", "and", "empowering", "people", "I", "teach", "HTML", "CSS", "JS", "React", "Python"]
+13 */
+
+/*
+let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.';
+
+let cleanSentence = text.replace(/\./g, '');
+cleanSentence = cleanSentence.replace(/,/g, '');
+
+let words = cleanSentence.split(' ');
+console.log(words);
+console.log(words.length);
+*/
+
+/* 3. In the following shopping cart add, remove, edit items
+
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+
+* Add 'Meat' in the beginning of your shopping cart if it has not been already added
+* Add Sugar at the end of you shopping cart if it has not been already added
+* Remove 'Honey' if you are allergic to honey
+* Modify Tea to 'Green Tea' */
+
+/*
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+console.log(shoppingCart);
+
+if (shoppingCart.includes('Meat')) {
+    console.log('You have meat in your cart already');
+} else {
+    shoppingCart.unshift('Meat');
+    console.log('Added meat succesfully!');
+}
+console.log(shoppingCart);
+
+if (shoppingCart.includes('Sugar')) {
+    console.log('You have sugar in your cart already');
+} else {
+    shoppingCart.push('Sugar');
+    console.log('Added sugar succesfully!');
+}
+console.log(shoppingCart);
+
+let AllergicToHoney = true;
+
+if (AllergicToHoney = false) {
+    shoppingCart.splice(shoppingCart.indexOf('Honey'), shoppingCart.indexOf('Sugar')+1);
+    console.log('You are allergic to honey, so I have removed it from your cart');
+} else {
+    console.log('Enjoy your honey!');
+}
+console.log(shoppingCart);
+
+shoppingCart.splice(shoppingCart.indexOf('Tea'), 1, 'Green Tea');
+console.log(shoppingCart);
+*/
+
+// 4. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+
+/*
+if (countries.includes('Ethiopia')) {
+    console.log('ETHIOPIA');
+} else {
+    countries.push('Ethiopia');
+    console.log('Added Ethiopia succesfully!');
+}
+console.log(countries);
+*/
+
+// 5. In the webTechs array check if Sass exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist add Sass to the array and print the array.
+
+/*
+if (webTechs.includes('Sass')) {
+    console.log('Sass is a CSS preprocess');
+} else {
+    webTechs.push('Sass');
+    console.log('Added Sass succesfully!');
+}
+console.log(webTechs);
+*/
+
+/* 6. Concatenate the following two variables and store it in a fullStack variable.
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+
+console.log(fullStack)
+
+["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"] */
+
+/*
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux'];
+const backEnd = ['Node','Express', 'MongoDB'];
+
+let fullStack = frontEnd.concat(backEnd);
+
+console.log(fullStack);
+*/
