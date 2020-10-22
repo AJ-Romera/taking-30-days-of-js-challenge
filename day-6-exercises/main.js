e66c5f619aa0f44b276d479812f8209032748de3
AJ-Romera <ajromeraalfonso@gmail.com>
2020-10-22 19:44:13 +0200

    Almost finish day 6 level 2 exercises

--- day-6-exercises/main.js
+++ day-6-exercises/main.js
@@ -255,3 +255,132 @@ for (let i = 0; i < 6; i++) {
 }
 console.log(randomChars);
 */
+
+
+// Exercises: Level 2
+
+// 1. Develop a small script which generate any number of characters random id:
+
+  /* fe3jo1gl124g
+  xkqci4utda1lmbelpkm03rba */
+
+/*
+let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+let randomChars = '';
+let n = parseInt(prompt('How long do you want the random id to be?'));
+for (let i = 0; i < n; i++) {
+    let random = Math.floor(Math.random() * chars.length);
+    randomChars += chars[random];
+}
+console.log(randomChars);
+*/
+
+// 2. Write a script which generates a random hexadecimal number.
+
+    // '#ee33df'
+
+/*
+let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+let randomChars = '';
+for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * chars.length);
+    randomChars += chars[random];
+}
+console.log(`${'#' + randomChars}`);
+*/
+
+// 3. Write a script which generates a random rgb color number.
+
+    // rgb(240,180,80)
+
+/*
+let rgbRandNums = []
+for (let i = 0; i < 3; i++) {
+    let random = (Math.floor(Math.random() * 256));
+    rgbRandNums.push(random)
+}
+console.log(`rgb(${rgbRandNums[0]}, ${rgbRandNums[1]}, ${rgbRandNums[2]})`); 
+*/
+
+// 4. Using the above countries array, create the following new array.
+
+    // ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"]
+
+/*
+const newArr = []
+for(let i = 0; i < countries.length; i++){
+    newArr.push(countries[i].toUpperCase());
+}
+console.log(newArr);
+*/
+    
+// 5. Using the above countries array, create an array for countries length'.
+
+    // [7, 7, 6, 7, 8, 7, 7, 7, 7, 5, 5]
+
+/*
+for(let i = 0; i < countries.length; i++){
+    console.log(countries[i].length);
+}
+*/
+
+// 6. Use the countries array to create the following array of arrays:
+
+  /* [
+  ['Albania', 'ALB', 7],
+  ['Bolivia', 'BOL', 7],
+  ['Canada', 'CAN', 6],
+  ['Denmark', 'DEN', 7],
+  ['Ethiopia', 'ETH', 8],
+  ['Finland', 'FIN', 7],
+  ['Germany', 'GER', 7],
+  ['Hungary', 'HUN', 7],
+  ['Ireland', 'IRE', 7],
+  ['Japan', 'JAP', 5],
+  ['Kenya', 'KEN', 5]
+] */
+
+
+
+// 7. In above countries array, check if there is a country or countries containing the word 'land'. If there are countries containing 'land', print it as array. If there is no country containing the word 'land', print 'All these countries are without land'.
+
+    // ['Finland', 'Iceland']
+
+// 8. In above countries array, check if there is a country or countries end with a substring 'ia'. If there are countries end with, print it as array. If there is no country containing the word 'ai', print 'These are countries ends without ia'.
+
+    // ['Albania', 'Bolivia','Ethiopia']
+
+// 9. Using the above countries array, find the country containing the biggest number of characters.
+
+    // Ethiopia
+
+// 10. Using the above countries array, find the country containing only 5 characters.
+
+// ['Japan', 'Kenya']
+
+// 11. Find the longest word in the webTechs array
+
+// 12. Use the webTechs array to create the following array of arrays:
+
+    // [["HTML", 4], ["CSS", 3],["JavaScript", 10],["React", 5],["Redux", 5],["Node", 4],["MongoDB", 7]]
+
+// 13. An application created using MongoDB, Express, React and Node is called a MERN stack app. Create the acronym MERN by using the array mernStack
+
+// 14. Iterate through the array, ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"] using a for loop or for of loop and print out the items.
+
+// 15. This is a fruit array , ['banana', 'orange', 'mango', 'lemon'] reverse the order using loop without using a reverse method.
+
+// 16. Print all the elements of array as shown below.
+
+  /* const fullStack = [
+    ['HTML', 'CSS', 'JS', 'React'],
+    ['Node', 'Express', 'MongoDB']
+  ]
+
+  HTML
+  CSS
+  JS
+  REACT
+  NODE
+  EXPRESS
+  MONGODB */
