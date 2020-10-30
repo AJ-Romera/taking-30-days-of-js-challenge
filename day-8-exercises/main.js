7c0f816082ccae3899d22412e6f3cb3d3cb8c92f
AJ-Romera <ajromeraalfonso@gmail.com>
2020-10-30 19:25:24 +0100

    Almost finish day 8 level 3 exercises

--- day-8-exercises/main.js
+++ day-8-exercises/main.js
@@ -198,3 +198,234 @@ ${countries[index].languages.join(', ')}`;
 console.log(print);
 */
 
+
+// Exercises: Level 3
+
+// 1. Create an object literal called personAccount. It has firstName, lastName, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo, addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is a set of incomes and its description.
+
+const personAccount = {
+	firstName: "Asabeneh",
+	lastName: 250,
+	incomes: {
+		incomes: [2000, 400],
+		description: ['Monthly salary', 'Monthly passive income'],
+	},
+	expenses: {
+		expenses: [300, 150, 250, 150],
+		description: ['Food', 'Water, electricity, gas, internet', 'Monthly insurance', 'Personal'],
+	},
+	totalIncome: function () {
+        let sum = 0;
+        for (let i = 0; i < this.incomes.incomes.length; i++) {
+            sum += this.incomes.incomes[i] ;
+        }
+		return `${sum}`;
+	},
+	totalExpense: function () {
+		let sum = 0;
+		for (let i = 0; i < this.expenses.expenses.length; i++) {
+			sum += this.expenses.expenses[i];
+		}
+		return `${sum}`;
+	},
+	addIncome: function (income, description) {
+		return this.incomes.incomes.push(income) && this.incomes.description.push(description);
+	},
+	addExpense: function (expenses, description) {
+		this.expenses.expenses.push(expenses) && this.expenses.description.push(description);
+	},
+	accountBalance: function () {
+		return this.totalIncome() - this.totalExpense();
+	}
+};
+
+personAccount.addIncome(10000, 'Successful app');
+console.log(personAccount.incomes);
+personAccount.addExpense(6000, "Pay to dev");
+console.log(personAccount.expenses);
+console.log(`You have a total income of ${personAccount.totalIncome()} this month`);
+console.log(`You have a total expense of ${personAccount.totalExpense()} this month`);
+console.log(`Balance this month: ${personAccount.accountBalance()}`);
+
+// 2. **** Questions: 2, 3 and 4 are based on the following two arrays:users and products
+// I renamed users array to appUsers, because we alredy have an users array.
+    const appUsers = [
+    {
+        _id: 'ab12ex',
+        username: 'Alex',
+        email: 'alex@alex.com',
+        password: '123123',
+        createdAt:'08/01/2020 9:00 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'fg12cy',
+        username: 'Asab',
+        email: 'asab@asab.com',
+        password: '123456',
+        createdAt:'08/01/2020 9:30 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'zwf8md',
+        username: 'Brook',
+        email: 'brook@brook.com',
+        password: '123111',
+        createdAt:'08/01/2020 9:45 AM',
+        isLoggedIn: true
+    },
+    {
+        _id: 'eefamr',
+        username: 'Martha',
+        email: 'martha@martha.com',
+        password: '123222',
+        createdAt:'08/01/2020 9:50 AM',
+        isLoggedIn: false
+    },
+    {
+        _id: 'ghderc',
+        username: 'Thomas',
+        email: 'thomas@thomas.com',
+        password: '123333',
+        createdAt:'08/01/2020 10:00 AM',
+        isLoggedIn: false
+    }
+    ];
+
+    const products = [
+  {
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+      { userId: 'fg12cy', rate: 5 },
+      { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+  },
+  {
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+  },
+  {
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+  }
+]
+
+// Imagine you are getting the above users collection from a MongoDB database.
+
+// a. Create a function called signUp which allows user to add to the collection. If user exists, inform the user that he has already an account.
+
+function signUp(username, email, password) {
+    let userExists = false; // This is a 'flag', it's the first time i use it
+    
+	for (let i = 0; i < appUsers.length; i++) {
+		if (appUsers[i].username === username || appUsers[i].email === email) {
+			console.log("User exists");
+			userExists = true;
+		}
+	}
+
+	if (!userExists) {
+		let id = [];
+		let chars = "abcdefghiklmnopqrstuvwxyz";
+		let date = new Date();
+		for (let i = 0; i < 6; i++) {
+			id.push(chars[Math.floor(Math.random() * chars.length)]);
+		}
+		id = id.join("");
+		appUsers.push({
+			_id: id,
+			username: username,
+			email: email,
+			password: password,
+			createdAt: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`,
+			isLoggedIn: "false",
+		});
+		console.log(
+			`User ${appUsers[appUsers.length - 1].username} created successfully`
+		);
+	}
+}
+signUp('María', "maría@maría.com", '123465');
+console.log(appUsers);
+
+// b. Create a function called signIn which allows user to sign in to the application
+
+function signIn(username, password) {
+    let index;
+    
+    for (const user of appUsers) {
+
+        if (user.username === username && user.password === password) {
+            console.log("User exists");
+            index = appUsers.indexOf(user); // 5
+        }
+        
+    }
+    if ((appUsers[index].isLoggedIn)) {
+        appUsers[index].isLoggedIn = true;
+        console.log(`${appUsers[index].username}, now you are logged in`);
+    }
+    
+    console.log(appUsers); // isLoggedIn now is true, you can check it in the console
+
+}
+signIn("María", "123465");
+
+// 3. The products array has three elements and each of them has six properties.
+// a. Create a function called rateProduct which rates the product
+
+function rateProduct() {
+
+}
+rateProduct();
+
+
+/* ONLY FOR REFERENCE, IGNORE
+const products = [
+	{
+		_id: "eedfcf",
+		name: "mobile phone",
+		description: "Huawei Honor",
+		price: 200,
+		ratings: [
+			{ userId: "fg12cy", rate: 5 },
+			{ userId: "zwf8md", rate: 4.5 },
+		],
+		likes: [],
+	},
+	{
+		_id: "aegfal",
+		name: "Laptop",
+		description: "MacPro: System Darwin",
+		price: 2500,
+		ratings: [],
+		likes: ["fg12cy"],
+	},
+	{
+		_id: "hedfcg",
+		name: "TV",
+		description: "Smart TV:Procaster",
+		price: 400,
+		ratings: [{ userId: "fg12cy", rate: 5 }],
+		likes: ["fg12cy"],
+	},
+];
+*/
+
+// b. Create a function called averageRating which calculate the average rating of a product
+
+
+
+// 4. Create a function called likeProduct. This function will helps to like to the product if it is not liked and remove like if it was liked.
