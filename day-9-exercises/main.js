a9ea604385ef4b9a45d2cd1958b9d9799f616282
AJ-Romera <ajromeraalfonso@gmail.com>
2020-11-04 19:40:02 +0100

    Almost finish day 9 level 1 exercises

---
+++ day-9-exercises/main.js
@@ -0,0 +1,149 @@
+// Exercises: Level 1
+
+
+    const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+    const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const products = [
+        { product: 'banana', price: 3 },
+        { product: 'mango', price: 6 },
+        { product: 'potato', price: ' ' },
+        { product: 'avocado', price: 8 },
+        { product: 'coffee', price: 10 },
+        { product: 'tea', price: '' }
+    ]
+
+// 1. Explain the difference between forEach, map, filter, and reduce.
+
+/*
+    forEach: Takes a callback function and run that callback function on each element of array one by one. Modifies the original array.
+
+    map: Returns a new array. The provided callback to map modifies the array elements and save them into the new array upon completion that array get returned as the mapped array.
+
+    filter: Filter out items and return a new array. Does not update the original array.
+
+    reduce: Is used to reduce the array to one single value.
+*/
+
+// 2. Define a call function before you them in forEach, map, filter or reduce.
+
+const priceLessSeven = products.filter((product) => product.price < 7);
+console.log(priceLessSeven); // Filter products with price less than 7
+
+// 3. Use forEach to console.log each country in the countries array.
+
+countries.forEach(country => console.log(country));
+
+// 4. Use forEach to console.log each name in the names array.
+
+names.forEach(name => console.log(name));
+
+// 5. Use forEach to console.log each number in the numbers array.
+
+numbers.forEach(num => console.log(num));
+
+// 6. Use map to create a new array by changing each country to uppercase in the countries array.
+
+const countriesToUppercase = countries.map(country => country.toUpperCase());
+console.log(countriesToUppercase);
+
+// 7. Use map to create an array of countries length from countries array.
+
+const countriesLength = countries.map(country => country.length);
+console.log(countriesLength);
+
+// 8. Use map to create a new array by changing each number to square in the numbers array
+
+const numSquare = numbers.map(num => num * num);
+console.log(numSquare);
+
+// 9. Use map to change to each name to uppercase in the names array
+
+names.map(name => console.log(name.toUpperCase()));
+
+// 10. Use map to map the products array to its corresponding prices.
+
+let productPrices = products.map((product) => {
+	return `${product.product}:${product.price}`;
+});
+console.log(productPrices);
+
+// 11. Use filter to filter out countries containing land.
+
+const countriesContainingLand = countries.filter((country) => country.toLowerCase().includes("land"));
+console.log(countriesContainingLand);
+
+// 12. Use filter to filter out countries having six character.
+
+const countriesHavingSixCharacters = countries.filter((country) => country.length === 6);
+console.log(countriesHavingSixCharacters);
+
+// 13. Use filter to filter out countries containing six letters and more in the country array.
+
+const countriesHavingSixCharactersOrMore = countries.filter((country) => country.length >= 6);
+console.log(countriesHavingSixCharactersOrMore);
+
+// 14. Use filter to filter out country start with 'E';
+
+const countriesStartsWithE = countries.filter((country) => country.toLowerCase().startsWith("e"));
+console.log(countriesStartsWithE);
+
+// 15. Use filter to filter out only prices with values.
+
+const pricesWithValueOnly = products.filter((product) => product.price > 0);
+console.log(pricesWithValueOnly);
+
+// 16. Declare a function called getStringLists which takes an array as a parameter and then returns an array only with string items.
+
+const getStringList = (arr) => {
+	let onlyStringArray = arr.filter((item) => {
+		return typeof item === "string";
+	});
+	return onlyStringArray;
+};
+
+let onlyStringArray = getStringList(["This is a string", 6, "Antonio", 7, 26]);
+console.log(onlyStringArray);
+
+// 17. Use reduce to sum all the numbers in the numbers array.
+
+let sumOfNums = numbers.reduce((accumulator, currentValue) => {
+	return accumulator + currentValue;
+});
+console.log(sumOfNums);
+
+// 18. Use reduce to concatenate all the countries and to produce this sentence: Estonia, Finland, Sweden, Denmark, Norway, and IceLand are north European countries
+
+
+
+// 19. Explain the difference between some and every
+
+
+
+// 20. Use some to check if some names' length greater than seven in names array
+
+
+
+// 21. Use every to check if all the countries contain the word land
+
+
+
+// 22. Explain the difference between find and findIndex.
+
+
+
+// 23. Use find to find the first country containing only six letters in the countries array
+
+
+
+// 24. Use findIndex to find the position of the first country containing only six letters in the countries array
+
+
+
+// 25. Use findIndex to find the position of Norway if it doesn't exist in the array you will get -1.
+
+
+
+// 26. Use findIndex to find the position of Russia if it doesn't exist in the array you will get -1.
+
+
