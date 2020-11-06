8f20c13530492f9f49df62664c7c56c82238bb18
AJ-Romera <ajromeraalfonso@gmail.com>
2020-11-06 21:31:36 +0100

    Finish day 9 level 3 exercises

--- day-9-exercises/main.js
+++ day-9-exercises/main.js
@@ -298,3 +298,172 @@ console.log(timesArray.indexOf(numOfTimesRepeated)); // The most repeated letter
 
 console.log(charAndTimesArray[20]); // S is the most repeated initial and is repeated 33 times.
 
+// Exercises: Level 3
+
+// 1. Use the countries information, in the data folder. Sort countries by name, by capital, by population
+
+/*
+// Sort by name
+countries2.sort((a, b) => {
+  if (a.name < b.name) return -1
+  if (a.name > b.name) return 1
+  return 0
+})
+console.log(countries2) // Sorted ascending
+*/
+
+/*
+// Sort by capital
+countries2.sort((a, b) => {
+  if (a.capital < b.capital) return -1
+  if (a.capital > b.capital) return 1
+  return 0
+})
+console.log(countries2) // Sorted ascending
+*/
+
+/*
+// Sort by population
+countries2.sort((a, b) => {
+  if (a.population < b.population) return -1
+  if (a.population > b.population) return 1
+  return 0
+})
+console.log(countries2) // Sorted descending
+*/
+
+// 2. *** Find the 10 most spoken languages:
+
+/*
+    // Your output should look like this
+    console.log(mostSpokenLanguages(countries, 10))
+    [
+    {country: 'English',count:91},
+    {country: 'French',count:45},
+    {country: 'Arabic',count:25},
+    {country: 'Spanish',count:24},
+    {country:'Russian',count:9},
+    {country:'Portuguese', count:9},
+    {country:'Dutch',count:8},
+    {country:'German',count:7},
+    {country:'Chinese',count:5},
+    {country:'Swahili',count:4}
+    ]
+
+    // Your output should look like this
+    console.log(mostSpokenLanguages(countries, 3))
+    [
+    {country: 'English',count: 91},
+    {country: 'French',count: 45},
+    {country: 'Arabic',count: 25},
+    ]```
+*/
+
+
+
+
+
+let languageAndCountArray = []
+let language = []
+
+function mostSpokenLanguages(countries2, n) {
+	for (let i = 0; i < countries2.length; i++) {
+		let languages = language.push(countries2[i].languages[0]);
+
+		let count = countries2.filter((country) =>
+			country.languages[0].includes(countries2[i].languages)
+		);
+
+		languageAndCountArray.push({ country: language[i], count: count.length });
+    }
+
+	languageAndCountArray.sort((a, b) => {
+		if (b.count < a.count) return -1;
+		if (b.count > a.count) return 1;
+		return 0;
+    });
+
+	return languageAndCountArray.splice(0, n); // sorted ascending
+};
+
+console.log(mostSpokenLanguages(countries2, 16)); // Something is wrong with duplicates, I have tried a lot of different things to remove duplicates, non of them worked so, Im done with this exercise.
+
+// 3. *** Use countries_data.js file create a function which create the ten most populated countries
+
+/*
+    console.log(mostPopulatedCountries(countries, 10))
+
+    [
+    {country: 'China', population: 1377422166},
+    {country: 'India', population: 1295210000},
+    {country: 'United States of America', population: 323947000},
+    {country: 'Indonesia', population: 258705000},
+    {country: 'Brazil', population: 206135893},
+    {country: 'Pakistan', population: 194125062},
+    {country: 'Nigeria', population: 186988000},
+    {country: 'Bangladesh', population: 161006790},
+    {country: 'Russian Federation', population: 146599183},
+    {country: 'Japan', population: 126960000}
+    ]
+
+    console.log(mostPopulatedCountries(countries, 3))
+    [
+    {country: 'China', population: 1377422166},
+    {country: 'India', population: 1295210000},
+    {country: 'United States of America', population: 323947000}
+    ]
+    ```
+*/
+
+nameAndPopulationArray = []
+
+function mostPopulatedCountries(countries2, n) {
+    for (let i = 0; i < countries2.length; i++) {
+        nameAndPopulationArray.push({country: countries2[i].name,population: countries2[i].population})
+    }
+
+    nameAndPopulationArray.sort((a, b) => {
+        if (b.population < a.population) return -1
+        if (b.population > a.population) return 1
+        return 0
+    })
+    return nameAndPopulationArray.splice(0, n); // sorted ascending
+};
+
+console.log(mostPopulatedCountries(countries2, 3));
+
+
+
+// 4. *** Try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create an object called statistics and create all the functions which do statistical calculations as method for the statistics object. Check the output below.
+
+/*
+    const ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+
+    console.log('Count:', statistics.count()) // 25
+    console.log('Sum: ', statistics.sum()) // 744
+    console.log('Min: ', statistics.min()) // 24
+    console.log('Max: ', statistics.max()) // 38
+    console.log('Range: ', statistics.range() // 14
+    console.log('Mean: ', statistics.mean()) // 30
+    console.log('Median: ',statistics.median()) // 29
+    console.log('Mode: ', statistics.mode()) // {'mode': 26, 'count': 5}
+    console.log('Variance: ',statistics.var()) // 17.5
+    console.log('Standard Deviation: ', statistics.std()) // 4.2
+    console.log('Variance: ',statistics.var()) // 17.5
+    console.log('Frequency Distribution: ',statistics.freqDist()) # [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+*/
+
+/*
+    console.log(statistics.describe())
+    Count: 25
+    Sum:  744
+    Min:  24
+    Max:  38
+    Range:  14
+    Mean:  30
+    Median:  29
+    Mode:  (26, 5)
+    Variance:  17.5
+    Standard Deviation:  4.2
+    Frequency Distribution: [(20.0, 26), (16.0, 27), (12.0, 32), (8.0, 37), (8.0, 34), (8.0, 33), (8.0, 31), (8.0, 24), (4.0, 38), (4.0, 29), (4.0, 25)]
+*/
