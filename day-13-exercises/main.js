e1d6ce463685b18f5ed826c4a14baa0934d045e7
AJ-Romera <ajromeraalfonso@gmail.com>
2020-11-10 12:35:21 +0100

    Finish day 13 level 2 exercises

--- day-13-exercises/main.js
+++ day-13-exercises/main.js
@@ -71,3 +71,17 @@ console.log("This is");
 console.log("an example of");
 console.log("groupCollapsed");
 console.groupEnd();
+
+
+// Exercises: Level 2
+// 1. 10 > 2 * 10 use console.assert()
+
+console.assert(10 > 2 * 10, 'Absolutely false!');
+
+// 2. Write a warning message using console.warn()
+
+console.warn('This is a manual warning message!');
+
+// 3. Write an error message using console.error()
+
+console.error('This is a manual error message! The colors of this errors and warning makes the flag of my country, Spain');
