f2410fa2044e668fb6645bb674fc99ed36d893f5
AJ-Romera <ajromeraalfonso@gmail.com>
2020-11-12 17:21:28 +0100

    Finish day 17 level 2 exercises

--- day-17-exercises/main.js
+++ day-17-exercises/main.js
@@ -1,4 +1,5 @@
 // Exercises: Level 1
+
 // 1. Store you first name, last name, age, country, city in your browser localStorage.
 
 localStorage.setItem("firstName", "Antonio");
@@ -6,3 +7,19 @@ localStorage.setItem("lastName", "Romera Alfonso");
 localStorage.setItem("age", 26);
 localStorage.setItem("country", "Spain");
 localStorage.setItem("city", "Cartaya");
+
+
+// Exercises: Level 2
+
+// 1. Create a student object. The student object will have first name, last name, age, skills, country, enrolled keys and values for the keys. Store the student object in your browser localStorage.
+
+let student = {
+	firstName: "Antonio José",
+	lastName: "Romera Alfonso",
+	age: 26,
+	skills: ["html", "css", "javascript", "Angular", "react", "git"],
+	country: "Spain"
+};
+
+let studentJSON = JSON.stringify(student);
+localStorage.setItem("student", studentJSON);
