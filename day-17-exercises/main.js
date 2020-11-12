83031f225f6c92f967d4de70c0ef67e467df793c
AJ-Romera <ajromeraalfonso@gmail.com>
2020-11-12 17:02:15 +0100

    Finish day 17 level 1 exercises

---
+++ day-17-exercises/main.js
@@ -0,0 +1,8 @@
+// Exercises: Level 1
+// 1. Store you first name, last name, age, country, city in your browser localStorage.
+
+localStorage.setItem("firstName", "Antonio");
+localStorage.setItem("lastName", "Romera Alfonso");
+localStorage.setItem("age", 26);
+localStorage.setItem("country", "Spain");
+localStorage.setItem("city", "Cartaya");
