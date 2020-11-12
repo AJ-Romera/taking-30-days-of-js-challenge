e48883435b8a42dfc6700f47ee0a8ecfc90daac2
AJ-Romera <ajromeraalfonso@gmail.com>
2020-11-12 12:01:02 +0100

    Finish day 16 level 1 exercises

---
+++ day-16-exercises/main.js
@@ -0,0 +1,128 @@
+const skills = ["HTML", "CSS", "JS", "React", "Node", "Python"];
+let age = 250;
+let isMarried = true;
+const student = {
+	firstName: "Asabeneh",
+	lastName: "Yetayehe",
+	age: 250,
+	isMarried: true,
+	skills: ["HTML", "CSS", "JS", "React", "Node", "Python"],
+};
+const txt = `{
+    "Alex": {
+        "email": "alex@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 30
+    },
+    "Asab": {
+        "email": "asab@asab.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Redux",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 25,
+        "isLoggedIn": false,
+        "points": 50
+    },
+    "Brook": {
+        "email": "daniel@daniel.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux"
+        ],
+        "age": 30,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Daniel": {
+        "email": "daniel@alex.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "Python"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "John": {
+        "email": "john@john.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Redux",
+            "Node.js"
+        ],
+        "age": 20,
+        "isLoggedIn": true,
+        "points": 50
+    },
+    "Thomas": {
+        "email": "thomas@thomas.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    },
+    "Paul": {
+        "email": "paul@paul.com",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "MongoDB",
+            "Express",
+            "React",
+            "Node"
+        ],
+        "age": 20,
+        "isLoggedIn": false,
+        "points": 40
+    }
+}
+`;
+
+// Exercises: Level 1
+
+// 1. Change skills array to JSON using JSON.stringify()
+
+const skillsJSON = JSON.stringify(skills, undefined, 4);
+console.log(skillsJSON);
+
+// 2. Stringify the age variable
+
+const ageJSON = JSON.stringify(age, undefined, 4);
+console.log(ageJSON);
+
+// 3. Stringify the isMarried variable
+
+const isMarriedJSON = JSON.stringify(isMarried, undefined, 4);
+console.log(isMarriedJSON);
+
+// 4. Stringify the student object
+
+const studentJSON = JSON.stringify(student, undefined, 4);
+console.log(studentJSON);
