da3c984843d0f73dee6eefb52b17581e6712ab5b
AJ-Romera <ajromeraalfonso@gmail.com>
2020-11-12 18:16:27 +0100

    Finish day 17 level 3 exercises

--- day-17-exercises/main.js
+++ day-17-exercises/main.js
@@ -23,3 +23,67 @@ let student = {
 
 let studentJSON = JSON.stringify(student);
 localStorage.setItem("student", studentJSON);
+
+
+// Exercises: Level 3
+
+// 1. Create an object called personAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
+
+let personAccount = {
+	firstName: "Antonio José",
+	lastName: "Romera Alfonso",
+	incomes: [
+		{ income: 3000, description: "Dev job" },
+		{ income: 250, description: "Passive income" },
+		{ income: 550, description: "Investment income" },
+	],
+	expenses: [
+		{ expense: 250, description: "Food" },
+		{ expense: 300, description: "Insurances" },
+		{ expense: 250, description: "Water, gas, electricity" },
+		{ expense: 25, description: "WiFi" },
+		{ expense: 100, description: "Clothes" },
+		{ expense: 50, description: "Gym" },
+		{ expense: 150, description: "Basic needs" }
+    ],
+    set addIncome(amount) {
+		this.incomes.push(amount);
+	},
+	set addExpenses(cost) {
+		this.expenses.push(cost);
+	},
+	totalIncome() {
+		let totalIncome = 0;
+		personAccount.incomes.forEach((incomeInfo) => (totalIncome += incomeInfo.income));
+		return totalIncome;
+	},
+	totalExpense() {
+		let totalExpense = 0;
+		this.expenses.forEach((expenseInfo) => (totalExpense += expenseInfo.expense));
+		return totalExpense;
+	},
+	accountBalance() {
+		let accBalance = this.totalIncome() - this.totalExpense();
+		return accBalance;
+	},
+	accountInfo() {
+        const accInfo = `Full name: ${this.firstName} ${this.lastName}\nTotal income: ${this.totalIncome()}\nTotal expenses: ${this.totalExpense()}\nBalance: ${this.accountBalance()}`;
+		return accInfo;
+	}
+};
+
+console.log(personAccount);
+
+personAccount.addIncome = { income: 200, description: "Crypto investment" };
+personAccount.addExpenses = { expense: 600, description: "New hardware" };
+
+console.log(personAccount.incomes);
+console.log(personAccount.expenses);
+
+console.log(personAccount.totalIncome());
+console.log(personAccount.totalExpense());
+
+console.log(personAccount.accountBalance());
+
+console.log(personAccount.accountInfo());
