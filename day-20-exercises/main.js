1b78c5707eca503d4616c5f50c3dca72a88e7534
AJ-Romera <ajromeraalfonso@gmail.com>
2020-11-14 18:22:36 +0100

    Finish day 20

---
+++ day-20-exercises/main.js
@@ -0,0 +1,12 @@
+/** Clean code
+ *
+ * This day is about clean code, it hasn't got exercises.
+ *
+ * The syntax of JavaScript has to be written following a certain style guideline for sake of convince and simplicity. It`s important when working in a team or when your code is going to be used by another person.
+ *
+ * Airbnb Javascript Style Guide:  https://github.com/airbnb/javascript
+ * Standard JavaScript Style Guide:  https://standardjs.com/
+ * Google JavaScript Style Guide:  https://google.github.io/styleguide/jsguide.html
+ * 
+ * I have read all of those and more.
+ */
