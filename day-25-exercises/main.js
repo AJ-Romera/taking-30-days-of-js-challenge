aafcee0de96a0cfab3982021cb53324c97ef663c
AJ-Romera <ajromeraalfonso@gmail.com>
2020-11-20 19:29:59 +0100

    Finish the challenge

---
+++ day-25-exercises/main.js
@@ -0,0 +1,10 @@
+// Exercise: Level 1
+
+// 1. Visualize the ten most populated countries and the ten most spoken languages in the world using DOM(HTML, CSS, JS)
+
+/**
+ * IMPORTANT: Im leaving the challenge here.
+ * Why?. Because days 25 to 30 are good projects to practice but they
+ * require a lot of time. I prefer spending time doing better projects.
+ * Maybe I make these exercises at some point in the furute.
+ *  */ 
